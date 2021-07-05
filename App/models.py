888a36f5ca4b32a1d8fa1ba5ca6b570660397228
Jalilov <javohirjalilov777@gmail.com>
2021-07-05 22:20:01 +0500

    Add Musician and Album Table

--- App/models.py
+++ App/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Language(models.Model):
 	name = models.CharField(max_length=30)
@@ -10,6 +10,23 @@ class Language(models.Model):
 class Framework(models.Model):
 	name = models.CharField(max_length=30)
 	language = models.ForeignKey(Language,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Musician(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	instrument = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.first_name
+
+class Album(models.Model):
+	artist = models.ForeignKey(Musician,on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	releaase_date = models.DateField(default=timezone.now)
+	num_stars = models.IntegerField()
 
 	def __str__(self):
 		return self.name
--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 **Framework Table**
 
 ![framework](https://github.com/JavohirJalilov/DjangoModels/raw/master/framework_table_show.png)
+
+**Musician Table**
+
+![musician](https://github.com/JavohirJalilov/DjangoModels/raw/master/musician_and_album.png)
+
+**Album Table**
+
+![album](https://github.com/JavohirJalilov/DjangoModels/raw/master/artist_table.png)
