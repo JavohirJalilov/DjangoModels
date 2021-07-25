ee807fbbcc26bb8e6ae6b415d5cc24e43b716a58
Jalilov <javohirjalilov777@gmail.com>
2021-07-25 13:36:51 +0500

    add author superuser

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post,Framework,Language,Musician,Album
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title','author','date_posted']
+
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Language)
+admin.site.register(Framework)
