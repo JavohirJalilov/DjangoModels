b962bcd154feb1e5b2aca3a858e2bded4c3a7167
Jalilov <javohirjalilov777@gmail.com>
2021-08-02 19:13:40 +0500

    rename

--- TodoApp/admin.py
+++ TodoApp/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Todo
 class TodoAdmin(admin.ModelAdmin):
-	list_display = ['title','date']
-	
+	list_display = ['taskname','date']
+
 admin.site.register(Todo,TodoAdmin)
